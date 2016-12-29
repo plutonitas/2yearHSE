7c7c58fa2e0df0c373baf45f08e53e3b02c8fb98
plutonitas <antoninayurievnas@gmail.com>
2016-12-29 13:28:19 +0300

    exam as is

---
+++ exam 29.12/exam 29.12 ho-ho-ho.py
@@ -0,0 +1,44 @@
+import urllib.request
+import re
+import html
+from pymystem3 import Mystem
+user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'
+
+def getText():
+    m = Mystem()
+    cyrillic = 'абвгдеёжзиклмнопрстуфхцчшщъыьэюя'
+    regTag = re.compile('<.*?>.*?', flags= re.DOTALL)
+    regTag2 = re.compile('.&nbsp;', flags = re.DOTALL)
+
+                
+    req = urllib.request.Request('http://web-corpora.net/Test2_2016/short_story.html',headers={'User-Agent':user_agent})
+    with urllib.request.urlopen(req) as response:
+        texthtml = response.read().decode('utf-8')
+        text = texthtml.split()
+        for word in text:
+            word = word.strip('\'\".,!?:;-—\(\)')
+            newword = re.sub(regTag,'',word)
+            newword = re.sub(regTag2,'',newword)
+
+            if newword and newword.lower()[0] in cyrillic:
+                    if newword.startswith('с'):
+                         #print(newword)
+                        morph = m.analyze(newword)
+                        #print(morph)
+                        noAnalysis = 0
+                        for item in morph:
+                            try:
+                                for parsing in item['analysis']:
+                                    if parsing['gr'] and ('V' in parsing['gr']):
+                                        print(array['text'])
+                            except:
+                                noAnalysis += 1
+                                    
+                                                                        
+                
+                
+                
+        
+
+getText()
+
